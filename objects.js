3e07f03369c6147ac0eafe4c8d4d762c27f6abf0
Natalie Halloran <natalie.halloran@yahoo.com>
2018-04-27 23:14:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
-var playlist = {}
+var artistName
+var songTitle
+var playlist = {artistName: songTitle}
 function updatePlaylist(playlist,artistName,songTitle) {
   playlist[artistName] = songTitle
   return playlist
